8918c955dc227d9d255d46c423137720ec99b4e4
joel <joelwilsonjam@gmail.com>
2024-01-06 20:50:04 -0500

    Initial commit

---
+++ src/test/java/com/joel/sshop/SshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joel.sshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
